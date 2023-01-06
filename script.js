75d47eceaf6f2c75eeb9f7f5a16861aa90b0ced4
Ayush Sharma <117670536+ayusharma-ctrl@users.noreply.github.com>
2023-01-07 03:19:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,16 +6,47 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const back = document.getElementById("back")
+const forward = document.getElementById("forward")
 
-toggle.addEventListener('click',toggleFunc)
-var count=0
-function toggleFunc(){
-    if(count==0){
+toggle.addEventListener('click', toggleFunc)
+var count = 0
+function toggleFunc() {
+    if (count == 0) {
         video.play()
-        count=1
+        count = 1
+        toggle.innerText = "❚ ❚"
     }
-    else{
+    else {
         video.pause()
-        count=0
+        count = 0
+        toggle.innerText = "►"
     }
+
 }
+
+function backing() {
+    console.log("hello")
+    video.currentTime += -10;
+}
+back.addEventListener('click', backing)
+
+function forwarding() {
+    console.log("hello")
+    video.currentTime += 25;
+}
+forward.addEventListener('click', forwarding)
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+video.addEventListener('timeupdate', handleProgress);
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+ranges.forEach(button => button.addEventListener('change', handleRangeUpdate));
+ranges.forEach(button => button.addEventListener('mousemove', handleRangeUpdate));
